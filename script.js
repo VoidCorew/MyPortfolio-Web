21c09179f0ce9c20c9f50b6caa477d2fd4ba4619
VoidCorew <morwe747@gmail.com>
2025-08-10 19:10:33 +0200

    commit

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ toggle.addEventListener('change', () => {
         localStorage.setItem('theme', 'light');
     }
 });
+
+// модальное окно
+const modal = document.getElementById("modal");
+const openBtns = document.querySelectorAll(".openModal");
+const closeBtn = modal.querySelector(".close");
+
+openBtns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();  // чтобы не прыгать вверх страницы
+    modal.style.display = "flex";
+  });
+});
+
+// openBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     modal.style.display = "flex"; // flex для центрирования
+// });
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Закрытие при клике вне окна
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
